c852b3f8fac5c3e2f9010fe6b4174f27e0174993
Sourabh Sihare <33661445+sourabhsihare@users.noreply.github.com>
2021-03-15 20:54:10 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region  = "ap-south-1"
   version = "~> 2.0"
-  access_key = "${AWS_ACCESS_KEY_ID}"
-  secret_key = "${AWS_SECRET_ACCESS_KEY}"
+  access_key = "${env.AWS_ACCESS_KEY_ID}"
+  secret_key = "${env.AWS_SECRET_ACCESS_KEY}"
 }
